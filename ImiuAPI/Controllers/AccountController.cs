4f21d792f27abcc8a235049ea4f3a989b3bce91c
duy-lvl <duylvlse160831@fpt.edu.vn>
2023-06-01 10:00:46 +0700

    rename apis

--- ImiuAPI/Controllers/AccountController.cs
+++ ImiuAPI/Controllers/AccountController.cs
@@ -19,52 +19,6 @@ public class AccountsController
 	}
 	
 	
-	/// <summary>
-	/// Register
-	/// </summary>
-	/// <param name="registerAccountModel"></param>
-	/// <returns></returns>
-	[HttpPost]
-	[Route("register")]
-	public IActionResult RegisterAccount([FromBody] RegisterAccountModel registerAccountModel)
-	{
-		var result = _accountService.RegisterAccount(registerAccountModel, false);
-		_unitOfWork.Commit();
-		var jsonResult = new JsonResult(result);
-		jsonResult.StatusCode = result.Status;
-		return jsonResult;
-	}
 	
-	/// <summary>
-	/// Verify email
-	/// </summary>
-	/// <param name="token"></param>
-	/// <returns></returns>
-	[HttpGet]
-	[Route("verify-email")]
-	public IActionResult VerifyEmail(string token)
-	{
-		var result = _accountService.VerifyEmail(token);
-		_unitOfWork.Commit();
-		var jsonResult = new JsonResult(result);
-		jsonResult.StatusCode = result.Status;
-		return jsonResult;
-	}
-	
-
-	/// <summary>
-	/// Send verify email
-	/// </summary>
-	/// <param name="email"></param>
-	/// <returns></returns>
-	[HttpPost]
-	[Route("email")]
-	public IActionResult SendEmail(string email)
-	{
-		var result = _accountService.SendEmail(email);
-		var jsonResult = new JsonResult(result);
-		jsonResult.StatusCode = result.Status;
-		return jsonResult;
-	}
 
 }
--- ImiuAPI/Controllers/AuthController.cs
+++ ImiuAPI/Controllers/AuthController.cs
@@ -1,10 +1,11 @@
 ﻿using DAL.UnitOfWork;
 using Microsoft.AspNetCore.Mvc;
 using Services.Service.Interface;
+using Services.ServiceModel;
 
 namespace ImiuAPI.Controllers;
 [ApiController]
-[Route("/api/v1")]
+[Route("/api/v1/auth")]
 public class AuthController : Controller
 {
     private readonly IAccountService _accountService;
@@ -15,7 +16,53 @@ public class AuthController : Controller
         _accountService = accountService;
         _unitOfWork = unitOfWork;
     }
+    /// <summary>
+    /// Register
+    /// </summary>
+    /// <param name="registerAccountModel"></param>
+    /// <returns></returns>
+    [HttpPost]
+    [Route("register")]
+    public IActionResult RegisterAccount([FromBody] RegisterAccountModel registerAccountModel)
+    {
+    	var result = _accountService.RegisterAccount(registerAccountModel, false);
+    	_unitOfWork.Commit();
+    	var jsonResult = new JsonResult(result);
+    	jsonResult.StatusCode = result.Status;
+    	return jsonResult;
+    }
     
+    /// <summary>
+    /// Verify email
+    /// </summary>
+    /// <param name="token"></param>
+    /// <returns></returns>
+    [HttpGet]
+    [Route("verify-email")]
+    public IActionResult VerifyEmail(string token)
+    {
+    	var result = _accountService.VerifyEmail(token);
+    	_unitOfWork.Commit();
+    	var jsonResult = new JsonResult(result);
+    	jsonResult.StatusCode = result.Status;
+    	return jsonResult;
+    }
+    
+
+    /// <summary>
+    /// Send verify email
+    /// </summary>
+    /// <param name="email"></param>
+    /// <returns></returns>
+    [HttpPost]
+    [Route("email")]
+    public IActionResult SendEmail(string email)
+    {
+    	var result = _accountService.SendEmail(email);
+    	var jsonResult = new JsonResult(result);
+    	jsonResult.StatusCode = result.Status;
+    	return jsonResult;
+    }
     /// <summary>
     /// Login with google use oauthIdToken
     /// </summary>
